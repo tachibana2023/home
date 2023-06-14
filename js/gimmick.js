292e5f99a220d3496f7111d898bc7cee6db6bdd8
tachibana2023 <131797989+tachibana2023@users.noreply.github.com>
2023-06-14 17:55:31 +0900

    update
    fix many things

--- js/gimmick.js
+++ js/gimmick.js
@@ -13,3 +13,31 @@ element.addEventListener('click', () => {
     }
     //コード見られたら元も子もないよね....
 });
+
+
+
+// KONAMIコマンド
+const secretCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let input = [];
+
+document.addEventListener('keydown', (e) => {
+    input.push(e.key);
+    input.splice(-secretCode.length - 1, input.length - secretCode.length);
+    if (input.join('') === secretCode.join('')) {
+        // ここに秘密のコードを実行する
+        console.log('KONAMIコマンド発動!');
+    }
+});
+
+
+
+//振ったら起動
+window.addEventListener('devicemotion', function (event) {
+    var x = event.accelerationIncludingGravity.x;
+    var y = event.accelerationIncludingGravity.y;
+    var z = event.accelerationIncludingGravity.z;
+
+    if (Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15) {
+        
+    }
+});
